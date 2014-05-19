0c4dcdb17c906e8ac26fa04a41ee702bc2b0ea23
Andrey Grebnev <andrey.grebnev@blandware.com>
2014-05-19 22:02:50 +0400

    Added additional methods

--- atleap-core/src/main/java/com/blandware/android/atleap/util/ActivityHelper.java
+++ atleap-core/src/main/java/com/blandware/android/atleap/util/ActivityHelper.java
@@ -1,11 +1,14 @@
 package com.blandware.android.atleap.util;
 
 import android.app.Activity;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.text.Spannable;
 import android.text.SpannableString;
 import android.text.TextUtils;
+import android.view.View;
+import android.widget.ProgressBar;
 
 /**
  * This class contains useful methods to work with Activity
@@ -13,15 +16,44 @@ import android.text.TextUtils;
 public class ActivityHelper {
 
     /**
-     * Change Progress Bar Indeterminate Visibility
+     * Change specified Progress Bar Visibility or Indeterminate Progress Bar
      * @param activity activity
      * @param show <code>true</code> if we would like to show
      */
     public static void changeProgressBarVisibility(Activity activity, boolean show) {
-        if (activity instanceof ActionBarActivity) {
-            final ActionBarActivity actionBarActivity = (ActionBarActivity) activity;
-            if (actionBarActivity != null) {
-                actionBarActivity.setSupportProgressBarIndeterminateVisibility(show);
+        ProgressBar progressBar = (ProgressBar)activity.findViewById(android.R.id.progress);
+        changeProgressBarVisibility(progressBar, activity, show);
+    }
+
+    /**
+     * Change specified Progress Bar Visibility or Indeterminate Progress Bar
+     * @param fragment fragment
+     * @param show <code>true</code> if we would like to show
+     */
+    public static void changeProgressBarVisibility(Fragment fragment, boolean show) {
+        ProgressBar progressBar = (ProgressBar)fragment.getView().findViewById(android.R.id.progress);
+        changeProgressBarVisibility(progressBar, fragment.getActivity(), show);
+    }
+
+    /**
+     * Change specified Progress Bar Visibility or Indeterminate Progress Bar
+     * @param progressBar progress bar, could be <code>null</code>
+     * @param show <code>true</code> if we would like to show
+     */
+    public static void changeProgressBarVisibility(ProgressBar progressBar, Activity activity, boolean show) {
+        if (progressBar != null) {
+            if (show)
+                progressBar.setVisibility(View.VISIBLE);
+            else
+                progressBar.setVisibility(View.GONE);
+        } else {
+            if (activity != null) {
+                if (activity instanceof ActionBarActivity) {
+                    ActionBarActivity actionBarActivity = (ActionBarActivity) activity;
+                    actionBarActivity.setSupportProgressBarIndeterminateVisibility(show);
+                } else {
+                    activity.setProgressBarIndeterminateVisibility(show);
+                }
             }
         }
     }
@@ -29,26 +61,25 @@ public class ActivityHelper {
     /**
      * Change title in the Action Bar
      * @param activity activity
-     * @param titleStringRes resource of String title
+     * @param title title of Action Bar
      */
-    public static void changeActionBarTitle(Activity activity, int titleStringRes) {
-        changeActionBarTitle(activity, titleStringRes, null);
+    public static void changeActionBarTitle(Activity activity, String title) {
+        changeActionBarTitle(activity, title, null);
     }
 
     /**
      * Change title in the Action Bar
      * @param activity activity
-     * @param titleStringRes resource of String title
+     * @param title title of Action Bar
      * @param typefaceName the full path to font in the assets
      */
-    public static void changeActionBarTitle(Activity activity, int titleStringRes, String typefaceName) {
+    public static void changeActionBarTitle(Activity activity, String title, String typefaceName) {
         if (!(activity instanceof ActionBarActivity))
             return;
 
         ActionBar actionBar = ((ActionBarActivity) activity).getSupportActionBar();
         if (actionBar != null) {
-            if (titleStringRes != 0) {
-                String title = activity.getString(titleStringRes);
+            if (!TextUtils.isEmpty(title)) {
                 if (!TextUtils.isEmpty(typefaceName)) {
                     SpannableString spannableTitle = new SpannableString(title);
                     spannableTitle.setSpan(new TypefaceSpan(activity, typefaceName), 0, spannableTitle.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
@@ -56,7 +87,6 @@ public class ActivityHelper {
                 } else {
                     actionBar.setTitle(title);
                 }
-
             } else {
                 actionBar.setTitle("");
             }
